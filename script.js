48c79e0ba25c18051de5265ffe5affe9e2c39253
JacksonWengler <jacksonwengler@gmail.com>
2026-03-20 17:30:20 -0500

    Added report editor with save and new report functionality, updated styles, and refined window management logic.

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ function updateTime() {
 setInterval(updateTime,1000)
 
 // Make the DIV element draggable:
-dragElement(document.querySelector("#welcome"));
-dragElement(document.querySelector("#report"));
-
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id + "header")) {
@@ -55,12 +52,7 @@ function dragElement(elmnt) {
 
 
 // Add opening and closing of windows
-var welcomeScreen = document.querySelector("#welcome")
-var welcomeScreenClose = document.querySelector("#welcomeclose")
-var welcomeScreenOpen = document.querySelector("#welcomeopen")
-var reportScreen = document.querySelector("#report")
-var reportScreenClose = document.querySelector("#reportclose")
-var reportScreenOpen = document.querySelector("#reportopen")
+var topBar = document.querySelector("#topbar")
 
 function closeWindow(element) {
     element.dataset.prevDisplay = getComputedStyle(element).display;
@@ -68,9 +60,171 @@ function closeWindow(element) {
 }
 function openWindow(element) {
     element.style.display = element.dataset.prevDisplay || "block";
+    biggestIndex ++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex + 1;
 }
 
-welcomeScreenClose.addEventListener("click", function () {closeWindow(welcomeScreen)})
-welcomeScreenOpen.addEventListener("click", function () {openWindow(welcomeScreen)})
-reportScreenClose.addEventListener("click", function () {closeWindow(reportScreen)})
-reportScreenOpen.addEventListener("click", function () {openWindow(reportScreen)})
+// Add it so windows rise to the top when clicked
+var biggestIndex = 1;
+
+function addWindowTapHandling(element) {
+    element.addEventListener("mousedown", () =>
+        handleWindowTap(element)
+    )
+}
+function handleWindowTap(element) {
+    biggestIndex ++;
+    element.style.zIndex = biggestIndex;
+    topBar.style.zIndex = biggestIndex+1;
+}
+
+//report editor functions
+// set the starting text of the report editor
+window.addEventListener("DOMContentLoaded", () => {
+    const editor = document.getElementById("reportEditor")
+
+    editor.value = `SOLARI CO. | {DEPARTMENT}
+{TITLE}
+
+Clearance Level: {REQUIRED CLEARANCE LEVEL}
+Prepared For: {IF NEEDED}
+Compiled By: {TIER-COLOR}
+Reference: {INCLUDE IF NEEDED}
+
+{SINGLE SMALL PARAGRAPH SUMMARIZING THE REPORT}
+
+{MAIN BODY}
+
+I. {SECTION I}
+Cras ut augue at arcu accumsan congue sit amet non.
+Vestibulum pellentesque metus: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis porta pulvinar. Suspendisse potenti. Curabitur maximus massa at congue venenatis.
+Donec nec tristique: Aliquam at ultrices nulla. Pellentesque convallis finibus cursus. Integer quis egestas massa. Pellentesque at lobortis enim.
+
+II. {SECTION II}
+{ADD MORE SECTIONS IF NEEDED
+
+SOLARI CO.
+{QUOTE FROM QUOTE LIST}
+    `
+
+})
+// new report button
+var newReportBut = document.querySelector("#reportNew")
+newReportBut.addEventListener("mousedown", () => {
+    var editor = document.getElementById("reportEditor")
+    var text = `SOLARI CO. | {DEPARTMENT}
+{TITLE}
+
+Clearance Level: {REQUIRED CLEARANCE LEVEL}
+Prepared For: {IF NEEDED}
+Compiled By: {TIER-COLOR}
+Reference: {INCLUDE IF NEEDED}
+
+{SINGLE SMALL PARAGRAPH SUMMARIZING THE REPORT}
+
+{MAIN BODY}
+
+I. {SECTION I}
+Cras ut augue at arcu accumsan congue sit amet non.
+Vestibulum pellentesque metus: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis porta pulvinar. Suspendisse potenti. Curabitur maximus massa at congue venenatis.
+Donec nec tristique: Aliquam at ultrices nulla. Pellentesque convallis finibus cursus. Integer quis egestas massa. Pellentesque at lobortis enim.
+
+II. {SECTION II}
+{ADD MORE SECTIONS IF NEEDED
+
+SOLARI CO.
+{QUOTE FROM QUOTE LIST}
+    `
+    if (editor.value === text) {
+        editor.value = `SOLARI CO. | {DEPARTMENT}
+{TITLE}
+
+Clearance Level: {REQUIRED CLEARANCE LEVEL}
+Prepared For: {IF NEEDED}
+Compiled By: {TIER-COLOR}
+Reference: {INCLUDE IF NEEDED}
+
+{SINGLE SMALL PARAGRAPH SUMMARIZING THE REPORT}
+
+{MAIN BODY}
+
+I. {SECTION I}
+Cras ut augue at arcu accumsan congue sit amet non.
+Vestibulum pellentesque metus: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis porta pulvinar. Suspendisse potenti. Curabitur maximus massa at congue venenatis.
+Donec nec tristique: Aliquam at ultrices nulla. Pellentesque convallis finibus cursus. Integer quis egestas massa. Pellentesque at lobortis enim.
+
+II. {SECTION II}
+{ADD MORE SECTIONS IF NEEDED
+
+SOLARI CO.
+{QUOTE FROM QUOTE LIST}
+    `
+    } else if (editor.value !== text) {
+        var userConfirmed = confirm("There are changes. Are you sure you want to create a new report?")
+
+        if (userConfirmed) {
+            editor.value = `SOLARI CO. | {DEPARTMENT}
+{TITLE}
+
+Clearance Level: {REQUIRED CLEARANCE LEVEL}
+Prepared For: {IF NEEDED}
+Compiled By: {TIER-COLOR}
+Reference: {INCLUDE IF NEEDED}
+
+{SINGLE SMALL PARAGRAPH SUMMARIZING THE REPORT}
+
+{MAIN BODY}
+
+I. {SECTION I}
+Cras ut augue at arcu accumsan congue sit amet non.
+Vestibulum pellentesque metus: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis porta pulvinar. Suspendisse potenti. Curabitur maximus massa at congue venenatis.
+Donec nec tristique: Aliquam at ultrices nulla. Pellentesque convallis finibus cursus. Integer quis egestas massa. Pellentesque at lobortis enim.
+
+II. {SECTION II}
+{ADD MORE SECTIONS IF NEEDED
+
+SOLARI CO.
+{QUOTE FROM QUOTE LIST}
+    `
+        }
+    }
+})
+// save button
+document.getElementById("reportSave").addEventListener("click", () => {
+    var text = document.getElementById("reportEditor").value
+    var blob = new Blob([text], {type: "text/plain"})
+    var url = URL.createObjectURL(blob)
+
+    var a = document.createElement("a")
+    a.href = url
+    a.download = "report.txt"
+    a.click()
+
+    URL.revokeObjectURL(url)
+})
+
+// helper functions
+function makeClosable(element) {
+    var openButton = document.querySelector("#" + element.id +"open")
+    var closeButton = document.querySelector("#" + element.id +"close")
+
+    closeButton.addEventListener("click", function () {
+        closeWindow(element)
+    })
+    openButton.addEventListener("click", function () {
+        openWindow(element)
+    })
+}
+function initializeWindow(element) {
+    var screen = document.querySelector("#" + element)
+    addWindowTapHandling(screen)
+    makeClosable(screen)
+    dragElement(screen)
+}
+initializeWindow("welcome")
+initializeWindow("report")
+
+//other functions
+
+
